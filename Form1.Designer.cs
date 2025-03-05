7a129fac72d83c93205690ccadd367b13eb1d0f6
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 17:16:34 +0700

    menambahkan implementasi kalkulator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            tambah = new Button();
+            button0 = new Button();
+            sama_dengan = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(button2, "button2");
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(button3, "button3");
+            button3.Name = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(button4, "button4");
+            button4.Name = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            resources.ApplyResources(button5, "button5");
+            button5.Name = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            resources.ApplyResources(button6, "button6");
+            button6.Name = "button6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            resources.ApplyResources(button7, "button7");
+            button7.Name = "button7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            resources.ApplyResources(button8, "button8");
+            button8.Name = "button8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            resources.ApplyResources(button9, "button9");
+            button9.Name = "button9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // tambah
+            // 
+            resources.ApplyResources(tambah, "tambah");
+            tambah.Name = "tambah";
+            tambah.UseVisualStyleBackColor = true;
+            tambah.Click += button10_Click;
+            // 
+            // button0
+            // 
+            resources.ApplyResources(button0, "button0");
+            button0.Name = "button0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button11_Click;
+            // 
+            // sama_dengan
+            // 
+            resources.ApplyResources(sama_dengan, "sama_dengan");
+            sama_dengan.Name = "sama_dengan";
+            sama_dengan.UseVisualStyleBackColor = true;
+            sama_dengan.Click += button12_Click;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label1);
+            Controls.Add(sama_dengan);
+            Controls.Add(button0);
+            Controls.Add(tambah);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button tambah;
+        private Button button0;
+        private Button sama_dengan;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,127 @@ namespace _103022300011_FRIZAM_DAFA_MAULANA
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        String value;
+        int hasil;
+        int hasil_terakhir=0; // tempat menampung hasil terakhir saat sudah melakukan operasi pertama kali
+        int angka1, angka2;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            value = "7";
+            label1.Text = value;
+            // button angka 7 yang akan mengeluarkan output kedalam "7" label
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            value = "1";
+            label1.Text = value;
+            // button angka 1 yang akan mengeluarkan output kedalam "1" label
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            value = "2";
+            label1.Text = value;
+            // button angka 2 yang akan mengeluarkan output kedalam "2" label
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            value = "3";
+            label1.Text = value;
+            // button angka 3 yang akan mengeluarkan output kedalam "3" label
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            value = "9";
+            label1.Text = value;
+            // button angka 9 yang akan mengeluarkan output kedalam "9" label
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            value = "4";
+            label1.Text = value;
+            // button angka 4 yang akan mengeluarkan output kedalam "4" label
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            value = "5";
+            label1.Text = value;
+            // button angka 5 yang akan mengeluarkan output kedalam "5" label
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            value = "6";
+            label1.Text = value;
+            // button angka 6 yang akan mengeluarkan output kedalam "6" label
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            value = "8";
+            label1.Text = value;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            angka1 = int.Parse(value);
+            label1.Text="";
+            // merefresh output di label dan mengkonvert value menjadi integer dan ditampung dalam variabel angka1
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            value = "0";
+            label1.Text = value;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            
+            angka2 = int.Parse(value);
+
+
+            if (hasil_terakhir == 0)
+            {
+                hasil = angka1 + angka2;
+                label1.Text = "" + hasil;
+                hasil_terakhir = hasil;
+
+            } else if (hasil_terakhir != 0) 
+            {
+                hasil_terakhir += angka2;
+                label1.Text = "" + hasil_terakhir;
+            }
+            // mengkonvert value yang akan ditampung di variabel angka2 dan melakukan operasi penjumlahan dan menampilkan hasilnya di label
+            // jika pertama kali melakukan operasi maka akan di tampung hasilnya kedalam hasil terakhir
+            //hasil terakhir digunakan jika masih ingin melakukan penjumlahan lagi
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
