7ad68fd3d752d53f91963b7b357536dd51e70c33
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 15:41:41 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _103022300011_FRIZAM_DAFA_MAULANA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
